70a29b2adf8dd064c3b7e13e952ff5c590b88f77
Vicky  T <vttak9@gmail.com>
2018-03-19 05:57:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  
+ 
+ get '/hello/:name.json' do
+   "Hello #{params[:name]}"
+ end
+
+
+
 end
